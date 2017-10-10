357a398d899d0863dfd31f76d3cce9be1f73a2bc
MOZGIII <mike-n@narod.ru>
2017-10-10 12:03:14 +0300

    Added UnmarshalJSON and MarshalJSON support for NullDecimal

--- decimal.go
+++ decimal.go
@@ -952,3 +952,21 @@ func (d NullDecimal) Value() (driver.Value, error) {
 	}
 	return d.Decimal.Value()
 }
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (d *NullDecimal) UnmarshalJSON(decimalBytes []byte) error {
+	if string(decimalBytes) == "null" {
+		d.Valid = false
+		return nil
+	}
+	d.Valid = true
+	return d.Decimal.UnmarshalJSON(decimalBytes)
+}
+
+// MarshalJSON implements the json.Marshaler interface.
+func (d NullDecimal) MarshalJSON() ([]byte, error) {
+	if !d.Valid {
+		return []byte("null"), nil
+	}
+	return d.Decimal.MarshalJSON()
+}

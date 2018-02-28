4c20285417111dcace15871397446ca1f5dbac64
Joseph Dallago <jd.dallago@gmail.com>
2018-02-28 17:45:29 +0000

    No longer trim trailing zeroes during initialization.  This ensures the precision is always retained.

--- decimal.go
+++ decimal.go
@@ -128,9 +128,8 @@ func NewFromString(value string) (Decimal, error) {
 		intString = value
 	} else if len(parts) == 2 {
 		// strip the insignificant digits for more accurate comparisons.
-		decimalPart := strings.TrimRight(parts[1], "0")
-		intString = parts[0] + decimalPart
-		expInt := -len(decimalPart)
+		intString = parts[0] + parts[1]
+		expInt := -len(parts[1])
 		exp += int64(expInt)
 	} else {
 		return Decimal{}, fmt.Errorf("can't convert %s to decimal: too many .s", value)

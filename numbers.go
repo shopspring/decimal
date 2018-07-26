8d3993cf620d2a7e6dbbd7242193ce5c34fc5b48
kempeng <39567537+kempeng@users.noreply.github.com>
2018-07-25 23:25:09 -0400

    Create numbers.go
    Numbers.go defines several values in decimal, extending on the Zero var already defined. These var's make the use of numbers value in decimal representation easier to use and read

---
+++ numbers.go
@@ -0,0 +1,55 @@
+package decimal
+
+// MinusOne defines value -1 in decimal format
+var MinusOne = New(-1, 0)
+
+// OneTenth defines value 0.1 in decimal format
+var OneTenth = New(1, -1)
+
+// TwoTenth defines value 0.1 in decimal format
+var TwoTenth = New(2, -1)
+
+// Half defines value 0.5 in decimal format
+var Half = New(5, -1)
+
+// One defines value 1 in decimal format
+var One = New(1, 0)
+
+// Two defines value 2 in decimal format
+var Two = New(2, 0)
+
+// Three defines value 3 in decimal format
+var Three = New(3, 0)
+
+// Four defines value 4 in decimal format
+var Four = New(4, 0)
+
+// Five defines value 5 in decimal format
+var Five = New(5, 0)
+
+// Six defines value 6 in decimal format
+var Six = New(6, 0)
+
+// Seven defines value 6 in decimal format
+var Seven = New(7, 0)
+
+// Eight defines value 8 in decimal format
+var Eight = New(8, 0)
+
+// Nine defines value 9 in decimal format
+var Nine = New(9, 0)
+
+// Ten defines value 10 in decimal format
+var Ten = New(10, 0)
+
+// Twenty defines value 20 in decimal format
+var Ten = New(20, 0)
+
+// Hundred defines value 100 in decimal format
+var Hundred = New(100, 0)
+
+// TwoHundred defines value 100 in decimal format
+var TwoHundred = New(200, 0)
+
+// Thousand defines value 100 in decimal format
+var Thousand = New(1000, 0)

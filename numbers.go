53965de0863676470b02a1726993ac9d0b39bb56
Geert van Kempen <geertvankempen@gmail.com>
2018-08-19 10:44:32 -0400

    additional decimal numbers

---
+++ numbers.go
@@ -0,0 +1,99 @@
+package common
+
+import "github.com/shopspring/decimal"
+
+// MinusOne defines value -1 in decimal format
+var MinusOne = decimal.New(-1, 0)
+
+// OneTenth defines value 0.1 in decimal format
+var OneTenth = decimal.New(1, -1)
+
+// TwoTenth defines value 0.1 in decimal format
+var TwoTenth = decimal.New(2, -1)
+
+// Half defines value 0.5 in decimal format
+var Half = decimal.New(5, -1)
+
+// One defines value 1 in decimal format
+var One = decimal.New(1, 0)
+
+// Two defines value 2 in decimal format
+var Two = decimal.New(2, 0)
+
+// Three defines value 3 in decimal format
+var Three = decimal.New(3, 0)
+
+// Four defines value 4 in decimal format
+var Four = decimal.New(4, 0)
+
+// Five defines value 5 in decimal format
+var Five = decimal.New(5, 0)
+
+// Six defines value 6 in decimal format
+var Six = decimal.New(6, 0)
+
+// Seven defines value 6 in decimal format
+var Seven = decimal.New(7, 0)
+
+// Eight defines value 8 in decimal format
+var Eight = decimal.New(8, 0)
+
+// Nine defines value 9 in decimal format
+var Nine = decimal.New(9, 0)
+
+// Ten defines value 10 in decimal format
+var Ten = decimal.New(10, 0)
+
+// Twenty defines value 20 in decimal format
+var Twenty = decimal.New(20, 0)
+
+// Thirty defines value 30 in decimal format
+var Thirty = decimal.New(30, 0)
+
+// Forty defines value 40 in decimal format
+var Forty = decimal.New(40, 0)
+
+// Fifty defines value 50 in decimal format
+var Fifty = decimal.New(50, 0)
+
+// Sixty defines value 60 in decimal format
+var Sixty = decimal.New(60, 0)
+
+// Seventy defines value 70 in decimal format
+var Seventy = decimal.New(70, 0)
+
+// Eighty defines value 80 in decimal format
+var Eighty = decimal.New(80, 0)
+
+// Ninty defines value 90 in decimal format
+var Ninty = decimal.New(90, 0)
+
+// Hundred defines value 100 in decimal format
+var Hundred = decimal.New(100, 0)
+
+// TwoHundred defines value 200 in decimal format
+var TwoHundred = decimal.New(200, 0)
+
+// ThreeHundred defines value 300 in decimal format
+var ThreeHundred = decimal.New(300, 0)
+
+// FourHundred defines value 400 in decimal format
+var FourHundred = decimal.New(400, 0)
+
+// FiveHundred defines value 500 in decimal format
+var FiveHundred = decimal.New(500, 0)
+
+// SixHundred defines value 600 in decimal format
+var SixHundred = decimal.New(600, 0)
+
+// SevenHundred defines value 700 in decimal format
+var SevenHundred = decimal.New(700, 0)
+
+// EightHundred defines value 800 in decimal format
+var EightHundred = decimal.New(800, 0)
+
+// NineHundred defines value 900 in decimal format
+var NineHundred = decimal.New(900, 0)
+
+// Thousand defines value 100 in decimal format
+var Thousand = decimal.New(1000, 0)

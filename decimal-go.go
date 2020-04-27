710e0d22af682cf132a1ece459ae8d203f39dd58
mwoss <wos.mateusz16@gmail.com>
2020-04-28 01:35:22 +0200

    Revert remove of docs

--- decimal-go.go
+++ decimal-go.go
@@ -2,6 +2,13 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Multiprecision decimal numbers.
+// For floating-point formatting only; not general purpose.
+// Only operations are assign and (binary) left/right shift.
+// Can do binary floating point in multiprecision decimal precisely
+// because 2 divides 10; cannot do decimal floating point
+// in multiprecision binary precisely.
+
 package decimal
 
 type decimal struct {
